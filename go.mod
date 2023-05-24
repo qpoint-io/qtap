c1beecd977eb281b5ee4bbc20da43f5bc517c32a
Tyler Flint <tylerflint@gmail.com>
2023-05-24 08:40:01 -0400

    Watch for version updates on ably

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/qpoint-io/qtap
 go 1.18
 
 require (
+	github.com/ably/ably-go v1.2.11 // indirect
 	github.com/alecthomas/kingpin/v2 v2.3.2 // indirect
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	github.com/klauspost/compress v1.10.3 // indirect
+	github.com/ugorji/go/codec v1.1.9 // indirect
 	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
 )
--- internal/watch/ably.go
+++ internal/watch/ably.go
@@ -1,14 +1,59 @@
 package watch
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/ably/ably-go/ably"
+)
+
 type Ably struct {
 	QpointID string
 	Token    string
+
+	// internal
+	chVersion   chan string
+	client      *ably.Realtime
+	unsubscribe func()
 }
 
 func (a *Ably) Watch() (chan string, error) {
-	return make(chan string), nil
+	// initialize the channel for sending version updates
+	a.chVersion = make(chan string)
+
+	// connect to realtime
+	client, err := ably.NewRealtime(ably.WithKey(a.Token))
+	if err != nil {
+		return nil, fmt.Errorf("connecting to ably realtime: %w", err)
+	}
+
+	// connect to the channel
+	channel := client.Channels.Get(fmt.Sprintf("qtap-%s", a.QpointID))
+
+	// listen for version updates
+	unsubscribe, err := channel.Subscribe(context.Background(), "version", func(msg *ably.Message) {
+		version, ok := msg.Data.(string)
+		if ok {
+			a.chVersion <- version
+		}
+	})
+	if err != nil {
+		return nil, fmt.Errorf("subscribing to realtime channel: %w", err)
+	}
+
+	// assign to struct
+	a.client = client
+	a.unsubscribe = unsubscribe
+
+	return a.chVersion, nil
 }
 
 func (a *Ably) Stop() error {
+	// unsubscribe
+	a.unsubscribe()
+
+	// disconnect
+	a.client.Close()
+
 	return nil
 }

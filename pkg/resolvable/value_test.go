a84876a43519215e18759a13b8e6499300c93fed
Kamal Nasser <kamal@qpoint.io>
2025-06-10 21:45:02 +0300

    fix tests

--- pkg/resolvable/value_test.go
+++ pkg/resolvable/value_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestValue_Resolve(t *testing.T) {
@@ -16,8 +16,8 @@ func TestValue_Resolve(t *testing.T) {
 			return 1, nil
 		})
 		value, err := v(ctx)
-		assert.NoError(t, err)
-		assert.Equal(t, 1, value)
+		require.NoError(t, err)
+		require.Equal(t, 1, value)
 	})
 
 	t.Run("retryable", func(t *testing.T) {
@@ -31,21 +31,21 @@ func TestValue_Resolve(t *testing.T) {
 		}, WithRetryable())
 
 		value, err := v(ctx)
-		assert.EqualError(t, err, "try again")
-		assert.Equal(t, 0, value)
+		require.EqualError(t, err, "try again")
+		require.Equal(t, 0, value)
 
 		value, err = v(ctx)
-		assert.EqualError(t, err, "try again")
-		assert.Equal(t, 0, value)
+		require.EqualError(t, err, "try again")
+		require.Equal(t, 0, value)
 
 		value, err = v(ctx)
-		assert.NoError(t, err)
-		assert.Equal(t, 3, value)
+		require.NoError(t, err)
+		require.Equal(t, 3, value)
 
 		// fn should not be called again
 		value, err = v(ctx)
-		assert.NoError(t, err)
-		assert.Equal(t, 3, value)
+		require.NoError(t, err)
+		require.Equal(t, 3, value)
 	})
 
 	t.Run("expiry", func(t *testing.T) {
@@ -57,19 +57,19 @@ func TestValue_Resolve(t *testing.T) {
 		}, WithExpiry(2*time.Second), WithNow(func() time.Time { return now }))
 
 		value, err := v(ctx)
-		assert.NoError(t, err)
-		assert.Equal(t, 1, value)
+		require.NoError(t, err)
+		require.Equal(t, 1, value)
 
 		// still not expired
 		now = now.Add(time.Second)
 		value, err = v(ctx)
-		assert.NoError(t, err)
-		assert.Equal(t, 1, value)
+		require.NoError(t, err)
+		require.Equal(t, 1, value)
 
 		// expired
 		now = now.Add(2 * time.Second)
 		value, err = v(ctx)
-		assert.NoError(t, err)
-		assert.Equal(t, 2, value)
+		require.NoError(t, err)
+		require.Equal(t, 2, value)
 	})
 }

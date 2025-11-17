0948ee87c31c86b0bdf2dcb9be1af6385c0f449f
Kamal Nasser <kamal@qpoint.io>
2025-11-17 21:52:31 +0200

    clean up preprocessing of service configs

--- pkg/services/manager.go
+++ pkg/services/manager.go
@@ -58,82 +58,50 @@ func (m *FactoryManager) SetConfig(cfg *config.Config) {
 		return
 	}
 
-	// get services from config
-	svcs := cfg.Services.All()
-	for _, fn := range m.extraServices {
-		// TODO(Jon): At this point we know that the services coming from the
-		// config have the appropriate defaults and IDs. So we can set the
-		// default flag to false and add an ID here instead of creating the
-		// seenServiceTypes set below.
-		svcs = append(svcs, fn(cfg))
+	plan, err := m.getServiceConfigs(cfg)
+	if err != nil {
+		m.logger.Error("failed to get service config plan, keeping existing config", zap.Error(err))
+		return
 	}
 
-	seenServiceTypes := set.NewSet[ServiceType]()
-	for i, svcConfig := range svcs {
-		fn := m.factoryFns.Get(ServiceType(svcConfig.Type))
-		if fn == nil {
-			m.logger.Debug("no factory registered for service", zap.String("service_type", svcConfig.Type))
-			continue
+	// initialize all factories
+	// bail out if any factory fails to initialize
+	for _, cfg := range plan {
+		if err := cfg.factory.Init(m.ctx, cfg.config); err != nil {
+			m.logger.Error("failed to initialize service, keeping existing config", zap.Stringer("service", cfg.factory.FactoryType()), zap.Error(err))
+			return // keep the existing config
 		}
+	}
 
-		factory := fn()
-
-		logger := m.logger.With(
-			zap.Stringer("service_type", factory.ServiceType()),
-			zap.String("factory_type", svcConfig.Type),
-		)
-		if svcConfig.ID != "" {
-			logger = logger.With(zap.String("service_id", svcConfig.ID))
-		}
-
-		var isDefault bool
-		if seenServiceTypes.Contains(factory.ServiceType()) {
-			// we have already seen this service type. give it an ID so that it is not set as default
-			isDefault = false
-			if svcConfig.ID == "" {
-				svcConfig.ID = fmt.Sprintf("service-%d", i)
+	// keep track of factories that need to be replaced with new instances
+	replacements := make(map[Factory]Factory)
+	for _, cfg := range plan {
+		keys := cfg.keys()
+		for _, key := range keys {
+			// check if there is an old factory for this key.
+			// keep the old factory reference in the replacements map as
+			// the registry entry will be replaced with the new factory
+			old := m.factories.Get(key)
+			if old != nil {
+				replacements[old] = cfg.factory
 			}
-		} else {
-			// we have not seen this service type yet. set it as default.
-			isDefault = true
-			seenServiceTypes.Add(factory.ServiceType())
-		}
 
-		logger.Info("initializing service factory")
-		if err := factory.Init(m.ctx, svcConfig.Config); err != nil {
-			logger.Error("failed to initialize service factory", zap.Error(err))
-			continue
+			// register the new factory for this key
+			m.factories.Register(cfg.factory, key.ID)
 		}
+	}
 
-		if svcConfig.ID != "" {
-			m.registerFactory(factory, svcConfig.ID)
-		}
-		// register it as the default if explicitly specified or no ID was provided
-		if isDefault {
-			m.registerFactory(factory, "")
-		}
+	// replace any old factories
+	for old, new := range replacements {
+		m.replaceFactory(old, new)
 	}
 }
 
-func (m *FactoryManager) registerFactory(factory Factory, id string) {
-	key := ServiceKey{Type: factory.ServiceType(), ID: id}
-	old := m.factories.Get(key)
-	if old != nil {
-		// gracefully replace the old factory if it exists
-		defer m.replaceFactory(id, old, factory)
-	}
-	m.factories.Register(factory, id)
-}
-
-func (m *FactoryManager) replaceFactory(id string, old Factory, new Factory) {
-	logger := m.logger.With(
+func (m *FactoryManager) replaceFactory(old Factory, new Factory) {
+	m.logger.Debug("replacing factory",
 		zap.Stringer("old_factory_type", old.FactoryType()),
-		zap.Stringer("new_factory_type", new.FactoryType()),
-	)
-	if id != "" {
-		logger = logger.With(zap.String("service_id", id))
-	}
-	logger.Debug("replacing factory")
+		zap.Stringer("new_factory_type", new.FactoryType()))
+
 	// send replacement factory to services that support it
 	if next, ok := old.(NextFactory); ok {
 		next.Next(new)
@@ -143,4 +111,80 @@ func (m *FactoryManager) replaceFactory(id string, old Factory, new Factory) {
 	if closer, ok := old.(io.Closer); ok {
 		closer.Close()
 	}
+}
+
+type serviceConfig struct {
+	isDefault bool
+	id        string
+	config    any
+	factory   Factory
+}
+
+func (c *serviceConfig) keys() []ServiceKey {
+	var keys []ServiceKey
+	if c.id != "" {
+		keys = append(keys, ServiceKey{Type: c.factory.ServiceType(), ID: c.id})
+	}
+	if c.isDefault {
+		keys = append(keys, ServiceKey{Type: c.factory.ServiceType()})
+	}
+	return keys
+}
+
+// getServiceConfigs creates a config plan given a user config.
+//
+// The following guarantees are made:
+//   - Each ServiceType has a default factory assigned.
+//     If one must be assigned by this function, the default factory will be the first instance we see of that ServiceType.
+//   - Each non-default service has an ID.
+//   - The order of the services in the config plan is the same as the order of the services in the user config, with extra services added at the end.
+func (m *FactoryManager) getServiceConfigs(cfg *config.Config) ([]*serviceConfig, error) {
+	// get all service configs from the user config + append any extra services
+	svcs := cfg.Services.All()
+	for _, fn := range m.extraServices {
+		svcs = append(svcs, fn(cfg))
+	}
+
+	// hasDefault keeps track of the service types that have a default factory assigned.
+	hasDefault := set.NewSet[ServiceType]()
+	cfgs := make([]*serviceConfig, 0, len(svcs))
+	for _, svc := range svcs {
+		fn := m.factoryFns.Get(ServiceType(svc.Type))
+		if fn == nil {
+			return nil, fmt.Errorf("no factory registered for service type: %s", svc.Type)
+		}
+		factory := fn()
+
+		if svc.Default {
+			hasDefault.Add(factory.ServiceType())
+		}
+		cfgs = append(cfgs, &serviceConfig{
+			isDefault: svc.Default,
+			id:        svc.ID,
+			factory:   factory,
+			config:    svc.Config,
+		})
+	}
+
+	// ensure that each ServiceType has a default factory assigned.
+	// services that are not default will be assigned an ID.
+	for i, cfg := range cfgs {
+		if cfg.isDefault {
+			hasDefault.Add(cfg.factory.ServiceType())
+			continue
+		}
+
+		if !hasDefault.Contains(cfg.factory.ServiceType()) {
+			// assigning the first instance we see of this service type as the default
+			hasDefault.Add(cfg.factory.ServiceType())
+			cfg.isDefault = true
+		}
+
+		if !cfg.isDefault && cfg.id == "" {
+			// ensure that all non-default services have an ID
+			cfg.id = fmt.Sprintf("service-%d", i)
+		}
+	}
+
+	return cfgs, nil
 }

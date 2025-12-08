41e39c7a88a8661fd6fd5e7fff18a6917e7d57f8
Kamal Nasser <kamal@qpoint.io>
2025-12-08 15:39:38 +0200

    trace process scan flow (#185)

--- pkg/cmd/tap_linux.go
+++ pkg/cmd/tap_linux.go
@@ -76,6 +76,8 @@ var (
 	persistentPlugins []config.Plugin
 )
 
+var tracer = telemetry.Tracer()
+
 func init() {
 	// Common options
 	rootCmd.Flags().StringVar(&qpointConfig, "config",
@@ -149,13 +151,16 @@ func init() {
 // This skeleton version of runrootCmd provides the basic structure
 // but will need to be fleshed out with actual implementation
 func runTapCmd(logger *zap.Logger) {
-	ctx := context.Background()
+	ctx, cancelRoot := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancelRoot()
 
 	shutdownTelemetry, err := setupTelemetry(ctx, "tap")
 	if err != nil {
 		logger.Fatal("unable to setup telemetry", zap.Error(err))
 	}
 	defer func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 		if err := shutdownTelemetry(ctx); err != nil {
 			logger.Error("unable to shutdown tracer provider", zap.Error(err))
 		}
@@ -485,10 +490,7 @@ func runTapCmd(logger *zap.Logger) {
 	logger.Info("eBPF program loaded and listening")
 
 	// trap int/term signals
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	<-sig
-
+	<-ctx.Done()
 	logger.Info("shutting down")
 }
 
@@ -550,6 +552,9 @@ func NewEbpfProcManager(logger *zap.Logger, objs *tap.TapObjects) (*ebpfProcess.
 }
 
 func InitTLSProbes(logger *zap.Logger, tlsProbesStr string, objs *tap.TapObjects) (*tls.TlsManager, error) {
+	ctx, span := tracer.Start(context.TODO(), "InitTLSProbes")
+	defer span.End()
+
 	// Split the string and trim whitespace
 	tlsProbesList := strings.Split(tlsProbesStr, ",")
 	for i, probe := range tlsProbesList {
@@ -576,7 +581,7 @@ func InitTLSProbes(logger *zap.Logger, tlsProbesStr string, objs *tap.TapObjects
 		ssl := tls.NewTlsManager(logger, probes...)
 
 		// start the tls probes manager
-		if err := ssl.Start(); err != nil {
+		if err := ssl.Start(ctx); err != nil {
 			logger.Fatal("failed to start tls probes manager", zap.Error(err))
 		}
 
